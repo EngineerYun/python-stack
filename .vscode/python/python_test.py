c26196f278982befba9e14a6f6a4ffd799ebfc87
EngineerYun <wateaful08@gmail.com>
2021-02-06 22:01:25 +0900

    python 첫 실행

---
+++ .vscode/python/python_test.py
@@ -0,0 +1,25 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # 이미지 가져오기
+# <img src='sea.jpg'>
+# %% [markdown]
+# # 리스트
+#  - 1번 리스트
+#  + 2번 리스트
+# %% [markdown]
+# |제목|내용|설명|
+# |:---|---:|:---:|
+# |왼쪽정렬|오른쪽정렬|중앙정렬|
+# |왼쪽정렬|오른쪽정렬|중앙정렬|
+# |왼쪽정렬|오른쪽정렬|중앙정렬|
+
+# %%
+print("Hello python")
+
+# %% [markdown]
+# 
+#  
+# 
+# 
+
